6b97aaa7613d58ae8c538fef993c924c1e4677db
unknown <mayurnavale96@gmail.com>
2020-10-15 23:32:05 -0700

    simple counter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MyCounterComponent } from './my-counter/my-counter.component';
+ 
 import { AppComponent } from './app.component';
-
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,MyCounterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({ count :counterReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
